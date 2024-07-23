31c935854286ee302c1c9ae4789fb1d815475621
Manoj Rawat <123074273+manojrawat123@users.noreply.github.com>
2024-07-23 10:35:20 +0530

    Update MonthSupport.jsx

--- src/pages/EmployeeMonthData/MonthSupport.jsx
+++ src/pages/EmployeeMonthData/MonthSupport.jsx
@@ -8,7 +8,7 @@ const MonthSupport = ({ data }) => {
         <div key={index} className={`py-4 rounded border my-3 text-center cursor-pointer hover:bg-gray-300 ${index % 2 === 0 ? '' : 'bg-gray-100'}`} >
           <h1 className='text-xl font-bold'>{date}:</h1>
           {attendance.leave ? <h1 style={{ color: "red" }}>Leave </h1> : <>
-            {attendance.checkinTime && !attendance.checkoutTime ?
+            {attendance.checkinTime && attendance.checkoutTime ?
             <div className=' items-center justify-center'>
               <h1>Checkin Time: {attendance.checkinTime ? attendance.checkinTime : ""}</h1>
               <h1>Checkout Time: {attendance.checkoutTime ? attendance.checkoutTime : ""}</h1>
