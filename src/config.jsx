d74f48797dc53a7be6e77b002b19a74f3f457c06
Manoj Rawat <123074273+manojrawat123@users.noreply.github.com>
2024-08-31 13:10:40 +0530

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,8 +1,8 @@
 // const API_BASE_URL = "http://192.168.29.78:8000";
 // const API_ROUTE_URL = "http://192.168.29.78:8000";  
 
-const API_BASE_URL = "https://kingbahimunna.pythonanywhere.com";
-const API_ROUTE_URL = "https://kingbahimunna.pythonanywhere.com";
+const API_BASE_URL = "https://vikassharma.pythonanywhere.com";
+const API_ROUTE_URL = "https://vikassharma.pythonanywhere.com";
 
 export {
     API_BASE_URL,
